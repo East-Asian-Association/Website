95d50d00fc3c6fa9e6737a744cc4e119ce8d5ad6
linn <linn.jagestedt@gmail.com>
2022-09-19 22:38:50 +0200

    format text

--- src/components/About/text_about.js
+++ src/components/About/text_about.js
@@ -3,21 +3,26 @@ export default function text() {
       <article>
           <h1>About</h1>
           <p>
-          The <b>East Asian Association</b>, <span class="chinese">林雪平大學東亞協會</span> (romaji: rinshōpin daigaku tōa kyōkai, pinyin: lín xuě píng dà xué dōng yà xié huì, hangul:임설평 대학 동아 협회) 
-          or EAA for short, is a student association at Linköping University that was founded the 28th of April 2009 as a collaboration by and for students interested in East Asia. 
-          Today the association has around <b>200 members</b> from all over the world, but primarily Sweden, Japan, China and Korea.
+            The East Asian Association, <span class="chinese">林雪平大學東亞協會</span>* 
+            or EAA for short, is a student association at Linköping University founded the 28th of April 2009 as a collaboration by and for students interested in East Asia. 
+          </p>
+          <p>
+            Today the association has around 200 members from all over the world, but primarily Sweden, Japan, China and Korea.
           </p>
           
           <p>        
-          We host a lot of events every year where some of the events are typical Swedish student life events and others are more focused on language and cultural exchange. 
-          We encourage everyone to join in these events and have as much fun as possible! If you are interested you can find more information under 
-          <b> Events</b>.
+            We host a lot of events every year where some of the events are typical Swedish student life events and others are more focused on language and cultural exchange. 
+            We encourage everyone to join in these events and have as much fun as possible! If you are interested you can find more information under 
+            Events.
           </p>
           
           <p> 
-          One of the main responsibilities for EAA is to host welcoming activities for new Swedish students coming to Linköping University to study Mandarin or Japanese combined with Engineering. 
-          (Sadly there is currently no program for studying the Korean language at Linköping University, 
-          but it is still a popular language among our members!)
+            One of the main responsibilities for EAA is to host welcoming activities for new Swedish students coming to Linköping University to study Mandarin or Japanese combined with Engineering. 
+            (Sadly there is currently no program for studying the Korean language at Linköping University, 
+            but it is still a popular language among our members!)
+          </p>
+          <p class="clarification">
+            <b>*Romaji:</b> rinshōpin daigaku tōa kyōkai, <b>Pinyin:</b> lín xuě píng dà xué dōng yà xié huì, <b>Hangul:</b> 임설평 대학 동아 협회
           </p>
       </article>
   );
