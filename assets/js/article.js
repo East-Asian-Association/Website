2cf08c7930014a3f717fe2d7e47a472daa7f16ff
linn <linn.jagestedt@gmail.com>
2022-09-27 00:33:54 +0200

    update

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,16 +1,18 @@
-function changePageContent(page) 
-{
-    $('#article-content').load(page + '.html');
-    $('.sidenav li').each(function() { $(this).removeClass('selected') }); $('#' + page).addClass('selected');
 
-    history.pushState({page: page}, page, "?page=" + page);
+function changePageContent(pageName) 
+{
+    $('#article-content').load(pageName + '.html');
+    $('.sidenav li').each(function() { $(this).removeClass('selected') }); $('#' + pageName).addClass('selected');
+
+    const state = { page: pageName }
+    history.pushState(state, null, "?page=" + pageName);
 };
 
-function initPage(defaultId) {
+function initPage(pageName) {
     var page = new URLSearchParams(window.location.search).get('page');
 
     if ($('#' + page).length == 0)
-      page = defaultId;
+      page = pageName;
       
     $('#article-content').load(page + '.html');
     $('.sidenav li').each(function() { $(this).removeClass('selected') }); $('#' + page).addClass('selected');
