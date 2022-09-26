d8111cc26f8f0034d8173855ee34f2e0b87befa2
linn <linn.jagestedt@gmail.com>
2022-09-27 00:28:22 +0200

    update

--- assets/js/article.js
+++ assets/js/article.js
@@ -3,8 +3,7 @@ function changePageContent(page)
     $('#article-content').load(page + '.html');
     $('.sidenav li').each(function() { $(this).removeClass('selected') }); $('#' + page).addClass('selected');
 
-    var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?page=" + page;
-    history.pushState({path:newUrl}, null, newUrl);
+    history.pushState({page: page}, page, "?page=" + page);
 };
 
 function initPage(defaultId) {
