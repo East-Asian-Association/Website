70fe0b596eed65bf031ceb5659fe6879a8d96ca1
linn <linn.jagestedt@gmail.com>
2022-09-30 21:47:36 +0200

    fix mistake

--- assets/js/article.js
+++ assets/js/article.js
@@ -3,13 +3,13 @@ function changeArticleContent(articleName)
     $('#article').load(articleName + '.html');
     $('.sidenav li').each(function() { $(this).removeClass('selected') }); $('#' + articleName).addClass('selected');
 
-    history.pushState({}, null, "?page=" + articleName);
+    history.pushState({}, null, "?article=" + articleName);
 };
 
 function initArticle(articleName) {
-    var article = new URLSearchParams(window.location.search).get('page');
+    var article = new URLSearchParams(window.location.search).get('article');
 
-    if ($('#' + page).length == 0)
+    if ($('#' + article).length == 0)
     article = articleName;
       
     $('#article').load(article + '.html');
