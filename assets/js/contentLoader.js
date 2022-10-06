1f0924f2dfc103542d4c167a51f2b0e116b1de69
linn <linn.jagestedt@gmail.com>
2022-10-06 14:48:03 +0200

    fix links

--- assets/js/contentLoader.js
+++ assets/js/contentLoader.js
@@ -20,35 +20,35 @@ function pushToHistory(articleName) {
     history.pushState({}, null, "?article=" + articleName);
 }
 
-function loadHeader(assetLocation) {
+function loadHeader(root) {
     $('#header').append(`
         <nav class="navbar">
             <a href='/' class="navbar-logo-wrapper">
-                <img class="navbar-logo" src="` + assetLocation + `svg/navbar-logo.svg"></img>
+                <img class="navbar-logo" src="` + root + `assets/svg/navbar-logo.svg"></img>
             </a>
 
             <button class="navbar-toggle" onClick='toggleNavbar()'>
-                <img class="navbar-toggle-icon" src="` + assetLocation + `svg/navbar-toggle-icon.svg"></img>
+                <img class="navbar-toggle-icon" src="` + root + `assets/svg/navbar-toggle-icon.svg"></img>
             </button>
 
             <ul class="black white-bg">
             <li>
-                <a class="redOnHover" href='/about'>About</a>
+                <a class="redOnHover" href='` + root + `about'>About</a>
             </li>
             <li>
-                <a class="redOnHover" href='/contact'>Contact</a>
+                <a class="redOnHover" href='` + root + `contact'>Contact</a>
             </li>
             <li>
-                <a class="redOnHover" href='/sponsors'>Sponsors</a>
+                <a class="redOnHover" href='` + root + `sponsors'>Sponsors</a>
             </li>
             <li>
-                <a class="redOnHover" href='/membership'>Membership</a>
+                <a class="redOnHover" href='` + root + `membership'>Membership</a>
             </li>
             <li>
-                <a class="redOnHover" href='/asienkunskap'>Asienkunskap</a>
+                <a class="redOnHover" href='` + root + `asienkunskap'>Asienkunskap</a>
             </li>
             <li>
-                <a class="redOnHover" href='/international'>International Students</a>
+                <a class="redOnHover" href='` + root + `international'>International Students</a>
             </li>
             </ul>
         </nav>
@@ -86,14 +86,14 @@ function loadFooter(assetLocation) {
             <div class='footer-grid-item'>
                 <h1 class='red'>EAA</h1>
                 <hr/>
-                <a href='https://www.facebook.com/eastasianassociation' class='social-media-button red-bg gray'><img src="` + assetLocation + `svg/facebook-icon.svg" /></a>
-                <a href='https://www.instagram.com/east_asian_association/' class='social-media-button red-bg gray'><img src="` + assetLocation + `svg/instagram-icon.svg" /></a>
+                <a href='https://www.facebook.com/eastasianassociation' class='social-media-button red-bg gray'><img src="` + root + `assets/svg/facebook-icon.svg" /></a>
+                <a href='https://www.instagram.com/east_asian_association/' class='social-media-button red-bg gray'><img src="` + root + `assets/svg/instagram-icon.svg" /></a>
             </div>
             <div class='footer-grid-item'>
                 <h1 class='red'>TEKKEN</h1>
                 <hr/>
-                <a href='https://www.facebook.com/FadderietTekken' class='social-media-button red-bg gray'><img src="` + assetLocation + `svg/facebook-icon.svg" /></a>
-                <a href='https://www.instagram.com/fadderiet_tekken/' class='social-media-button red-bg gray'><img src="` + assetLocation + `svg/instagram-icon.svg" /></a>
+                <a href='https://www.facebook.com/FadderietTekken' class='social-media-button red-bg gray'><img src="` + root + `assets/svg/facebook-icon.svg" /></a>
+                <a href='https://www.instagram.com/fadderiet_tekken/' class='social-media-button red-bg gray'><img src="` + root + `assets/svg/instagram-icon.svg" /></a>
             </div>
         </div>
     `);
